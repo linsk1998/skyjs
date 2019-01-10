461f13bbb79bf504c3f76b4bb8b2381f0fc8cfd1
linsk1998 <h@linsk.us>
2019-01-10 22:10:15 +0800

    document.currentScript

--- js/script.js
+++ js/script.js
@@ -32,80 +32,118 @@ Sky.getScript=function(src,func,charset){
 	return script;
 };
 (function(){
-	Sky.support.getCurrentPath=true;
+	var nodes=document.getElementsByTagName('SCRIPT');
+	var currentScript=nodes[nodes.length-1];
 	Sky.support.getCurrentScript=true;
 	if("currentScript" in document){//最新浏览器
-		Sky.getCurrentScript=function(){
-			return document.currentScript;
-		};
 	}else{
-		var currentScript;
-		Sky.getCurrentScript=function(){//IE
-			var nodes=document.getElementsByTagName('SCRIPT');
-			var i=nodes.length;
-			while(i--){
-				var node=nodes[i];
-				if(node.readyState==="interactive") {
-					return node;
+		if("readyState" in currentScript){
+			Sky.getCurrentScript=function(){//IE11-
+				var nodes=document.getElementsByTagName('SCRIPT');
+				var i=nodes.length;
+				while(i--){
+					var node=nodes[i];
+					if(node.readyState==="interactive") {
+						return node;
+					}
 				}
+				return null;
+			};
+			if(Object.defineProperty){
+				Object.defineProperty(document,"currentScript",{
+					enumerable:!!Object.defineProperties,//IE8不支持enumerable
+					get:function(){
+						return Sky.getCurrentScript();
+					}
+				});
+			}else{
+				
 			}
-		};
-		currentScript=Sky.getCurrentScript();
-		if(!currentScript){//非IE的低版本
-			try{
-				throw new Error('get stack');
-			}catch(e){
-				var stackHandler={
-					'stack':[
-						/^@(.*):\d+$/,// Firefox
-						/^\s+at (.*):\d+:\d+$/,//Chrome
-						/^\s+at [^\(]*\((.*):\d+:\d+\)$/ //IE11
-					],
-					'stacktrace':[
-						/\(\) in\s+(.*?\:\/\/\S+)/m//opera
-					]
-				};
-				var stackResult=handleStack(e,stackHandler);
-				if(stackResult){
-					Sky.getCurrentPath=function(){
-						try{
-							throw new Error('get stack');
-						}catch(e){
-							var arr=getLastStack(e[stackResult.name]).match(stackResult.pattern);
-							if(arr && arr[1]!=location.href){
-								return arr[1];
-							}
-						}
-					};
-					//同时加载多个相同的js会有问题
-					Sky.support.getCurrentScript=false;
-					Sky.getCurrentScript=function(){
-						var nodes=(Sky.isReady?document.head:document).getElementsByTagName('SCRIPT');
-						var i=nodes.length;
-						var currentPath=Sky.getCurrentPath();
-						if(currentPath){
-							while(i--){
-								var node=nodes[i];
-								if(new URL(node.src,location).href==currentPath) {
-									return node;
-								}
-							}
-						}else{
-							return nodes[nodes.length-1];
-						}
-					};
-				}else{//同时加载多个js会有问题
-					Sky.support.getCurrentScript=false;
-					Sky.support.getCurrentPath=false;
-					Sky.getCurrentScript=function(){
-						var nodes=(Sky.isReady?document.head:document).getElementsByTagName('SCRIPT');
-						return nodes[nodes.length-1];
-					};
+		}else if("onbeforescriptexecute" in currentScript){
+			document.currentScript=currentScript;
+			document.addEventListener('beforescriptexecute',function(e){
+				document.currentScript=e.target;
+			},true);
+			document.addEventListener('afterscriptexecute',function(e){
+				document.currentScript=null;
+			},true);
+		}else{
+			document.addEventListener('load',function(e){
+				if(e.target.tagName==="SCRIPT"){
+					e.target.readyState="complete";
 				}
+			},true);
+			if(Sky.browser.ie11){//ie11的script可以触发onload
+				Object.defineProperty(document,"currentScript",{
+					enumerable:true,
+					get:function(){
+						var nodes=document.getElementsByTagName('SCRIPT');
+						var i=nodes.length;
+						while(i--){
+							var node=nodes[i];
+							if(node.readyState!=="complete") {
+								return node;
+							}
+						}
+						return null;
+					}
+				});
+			}else{//非IE的低版本无法完美获取
+				Sky.support.getCurrentScript=false;
+				Object.defineProperty(document,"currentScript",{
+					enumerable:true,
+					get:function(){
+						var parent=(Sky.isReady?document.head:document);
+						var nodes=parent.getElementsByTagName('SCRIPT');
+						var last=nodes[nodes.length-1];
+						if(last.readyState==="complete") {
+							return null;
+						}
+						if(last.src){
+							return last;
+						}
+						setTimeout(function(){last.readyState="complete";},0);
+						return last;
+					}
+				});
 			}
 		}
 	}
+	if(!Sky.getCurrentScript){//最新浏览器
+		Sky.getCurrentScript=function(){
+			return document.currentScript;
+		};
+	}
+	Sky.support.getCurrentPath=true;
+	try{
+		throw new Error('get stack');
+	}catch(e){
+		var stackHandler={
+			'stack':[
+				/^@(.*):\d+$/,// Firefox
+				/^\s+at (.*):\d+:\d+$/,//Chrome
+				/^\s+at [^\(]*\((.*):\d+:\d+\)$/ //IE11
+			],
+			'stacktrace':[
+				/\(\) in\s+(.*?\:\/\/\S+)/m//opera
+			]
+		};
+		var stackResult=handleStack(e,stackHandler);
+		if(stackResult){
+			Sky.getCurrentPath=function(){
+				try{
+					throw new Error('get stack');
+				}catch(e){
+					var arr=getLastStack(e[stackResult.name]).match(stackResult.pattern);
+					if(arr && arr[1]!=location.href){
+						return arr[1];
+					}
+				}
+			};
+		}
+	}
 	if(!Sky.getCurrentPath){
+		Sky.support.getCurrentPath=false;
 		Sky.getCurrentPath=function(){
 			var currentScript=Sky.getCurrentScript();
 			return new URL(currentScript.src,location).href;
