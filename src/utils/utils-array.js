973720158faec0133c574a9af1c57323de0fe8a4
linsk1998 <h@linsk.us>
2019-12-29 21:02:41 +0800

    增加交集并集差集工具函数

--- src/utils/utils-array.js
+++ src/utils/utils-array.js
@@ -68,3 +68,50 @@ Sky.sortedLastIndex=function(arr,value){
 		}
 	}
 };
+Sky.union=function(){
+	var set=new Set();
+	for(var i=0;i<arguments.length;i++){
+		var arr=arguments[i];
+		if(!Array.isArray(arr)){
+			arr=Array.from(arr);
+		}
+		var j=arr.length;
+		while(j-->0){
+			set.add(arr[j]);
+		}
+	}
+	return Array.from(set);
+};
+Sky.difference=function(arg1){
+	if(arguments.length===0){
+		return new Array();
+	}
+	var set=new Set(arg1);
+	for(var i=1;i<arguments.length;i++){
+		var arr=arguments[i];
+		if(!Array.isArray(arr)){
+			arr=Array.from(arr);
+		}
+		var j=arr.length;
+		while(j-->0){
+			set['delete'](arr[j]);
+		}
+	}
+	return Array.from(set);
+};
+Sky.intersection=function(arg1){
+	if(arguments.length===0){
+		return new Array();
+	}
+	var set=new Set(arg1);
+	for(var i=1;i<arguments.length;i++){
+		var arr=arguments[i];
+		if(!Array.isArray(arr)){
+			arr=Array.from(arr);
+		}
+		set.forEach(function(item){
+			if(arr.indexOf(item)<0) this['delete'](item);
+		},set);
+	}
+	return Array.from(set);
+};
